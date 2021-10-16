2792547158d2e43204143bf88118a2457b121338
Christian Aashamar <chrizzy89@gmail.com>
2021-10-16 23:31:28 +0200

    Updated about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,38 +1,28 @@
+import { InlineLink } from "../InlineLink";
 import styles from "./AboutMe.module.scss";
-
-const getAge = (dateString: string) => {
-  var today = new Date();
-  var birthDate = new Date(dateString);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
-};
 
 export const AboutMe = () => {
   return (
     <div className="section" id="about">
       <div className={styles.aboutMeContainer}>
+        <h3>Hey, my name is</h3>
         <h1>Christian Aashamar</h1>
+        <h2>Software engineer</h2>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
-          molestias repudiandae ad fuga ratione distinctio pariatur nesciunt
-          laudantium cum? Veritatis exercitationem repudiandae natus, labore
-          mollitia ullam accusantium alias eligendi numquam.
+          I am a passionate and creative person that learns fast, is solution
+          oriented and execution. I have always been very interested in IT,
+          especially programming and other creative work in gaming, animation
+          and film. I have used a lot of my free time to learn and evolve in
+          these areas. It was therefore natural for me to go to scholl as a
+          computer engineer and it is amazing to work with what I love to do.
         </p>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
-          molestias repudiandae ad fuga ratione distinctio pariatur nesciunt
-          laudantium cum? Veritatis exercitationem repudiandae natus, labore
-          mollitia ullam accusantium alias eligendi numquam.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
-          molestias repudiandae ad fuga ratione distinctio pariatur nesciunt
-          laudantium cum? Veritatis exercitationem repudiandae natus, labore
-          mollitia ullam accusantium alias eligendi numquam.
+          I started in early/middle 2000s by creating simple websites for
+          personal use using HTML and CSS. This eventually got me into creating
+          websites with PHP and since 2018 I have mostly focused on building
+          websites with React and Node.js, which is one of the things I do at
+          &nbsp;
+          <InlineLink to="https://arctic.com">Arctic</InlineLink>.
         </p>
       </div>
     </div>
